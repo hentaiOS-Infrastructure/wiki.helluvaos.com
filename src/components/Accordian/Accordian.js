006f08ddfff653a567c81d5d3dc1b759498086b1
Rohan Hasabe <rohanhasabe8@gmail.com>
2021-10-04 21:54:13 +0530

    Dark theme fixes and populated the accordion (#12)
    * dark theme fixes
    
    * Populate the accordion and correct the theme on the arrows of the accordion
    
    Co-authored-by: Charudutta <restrictedcharudutta@gmail.com>

--- src/components/Accordian/Accordian.js
+++ src/components/Accordian/Accordian.js
@@ -5,6 +5,7 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
 import { MdKeyboardArrowUp } from "react-icons/md";
+import useThemeContext from "@theme/hooks/useThemeContext";
 import "./Accordian.css";
 import { data } from "./data";
 
@@ -33,6 +34,7 @@ const useStyles = makeStyles((theme) => ({
 const Accordian = () => {
   const classes = useStyles();
   const [expanded, setExpanded] = useState(false);
+  const { isDarkTheme } = useThemeContext();
 
   const handleChange = (panel) => (e, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
@@ -57,29 +59,43 @@ const Accordian = () => {
                       border: "1px solid #CECECE",
                     }}
                   >
-                    <AccordionSummary
-                      style={{ color: "#1C1E21" }}
-                      expandIcon={
-                        <MdKeyboardArrowUp
-                          style={{ color: "#1C1E21", fontSize: "27px" }}
-                        />
-                      }
-                      aria-controls="panel1a-content"
-                      id="panel1a-header"
+                    <div
+                      className={isDarkTheme ? "accord-dark" : "accord-light"}
                     >
-                      <h3 className="faq-question">
-                        <i
-                          className="fa fa-question-circle"
-                          aria-hidden="true"
-                        ></i>
-                        &nbsp; &nbsp;{item.question}
-                      </h3>
-                    </AccordionSummary>
-                    <AccordionDetails>
-                      <Typography style={{ color: "#1C1E21" }}>
-                        {item.answer}
-                      </Typography>
-                    </AccordionDetails>
+                      <AccordionSummary
+                        style={{ color: "#1C1E21" }}
+                        expandIcon={
+                          <div
+                            className={
+                              isDarkTheme ? "accord-dark" : "accord-light"
+                            }
+                          >
+                            <MdKeyboardArrowUp
+                              style={{ color: "#c60043", fontSize: "27px" }}
+                            />
+                          </div>
+                        }
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                      >
+                        <h3 className="faq-question">
+                          <i
+                            className="fa fa-question-circle"
+                            aria-hidden="true"
+                          ></i>
+                          &nbsp; &nbsp;{item.question}
+                        </h3>
+                      </AccordionSummary>
+                    </div>
+                    <div className={isDarkTheme ? "accord-dark" : null}>
+                      <AccordionDetails>
+                        <Typography style={{ color: "#1C1E21" }}>
+                          <div id={isDarkTheme ? "white-text" : null}>
+                            {item.answer}
+                          </div>
+                        </Typography>
+                      </AccordionDetails>
+                    </div>
                   </Accordion>
                 </div>
               </>
--- src/components/Accordian/data.js
+++ src/components/Accordian/data.js
@@ -1,26 +1,26 @@
 export const data = [
-  {
-    panel: "panel1",
-    question: "Where can I watch Helluva Boss?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad labore quaerat exercitationem eaque dolores pariatur!",
-  },
+  // {
+  //   panel: "panel1",
+  //   question: "Where can I watch Helluva Boss?",
+  //   answer:
+  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad labore quaerat exercitationem eaque dolores pariatur!",
+  // },
   {
     panel: "panel2",
     question: "Why?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad labore quaerat exercitationem eaque dolores pariatur!",
+      '"Yes"',
   },
   {
     panel: "panel3",
     question: "How do I get this thing?",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad labore quaerat exercitationem eaque dolores pariatur!",
+      "https://t.me/hentaiOS",
   },
   {
     panel: "panel4",
     question: "Get support for your lewd phone",
     answer:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad labore quaerat exercitationem eaque dolores pariatur!",
+      "Refer to your device maintainers",
   },
 ];
