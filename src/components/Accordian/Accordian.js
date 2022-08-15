c1cf58555dc24be30e3c8262421a0db69c25853d
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2022-08-15 13:11:50 +0700

    Accordian: Fix React deprecated functions warnings
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- src/components/Accordian/Accordian.js
+++ src/components/Accordian/Accordian.js
@@ -5,7 +5,7 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
 import { MdKeyboardArrowUp } from "react-icons/md";
-import {useColorMode} from '@docusaurus/theme-common';
+import { useColorMode } from '@docusaurus/theme-common';
 import "./Accordian.css";
 import { data } from "./data";
 
@@ -34,7 +34,6 @@ const useStyles = makeStyles((theme) => ({
 const Accordian = () => {
   const classes = useStyles();
   const [expanded, setExpanded] = useState(false);
-  const { isDarkTheme } = useColorMode();
 
   const handleChange = (panel) => (e, isExpanded) => {
     setExpanded(isExpanded ? panel : false);
@@ -46,61 +45,59 @@ const Accordian = () => {
       <div className="faq">
         <div className="faq-block">
           <h2 className="accord-title">Get started with your thing.</h2>
-          {data.map((item) => {
-            return (
-              <>
-                <div className="accordian-container">
-                  <Accordion
-                    className={`accord-dark ${classes.accord}`}
-                    expanded={expanded === item.panel}
-                    onChange={handleChange(`${item.panel}`)}
-                    style={{
-                      margin: "0",
-                      border: "1px solid #CECECE",
-                    }}
-                  >
-                    <div
-                      className={isDarkTheme ? "accord-dark" : "accord-light"}
+          {React.Children.toArray(
+            data.map((item) => {
+              return (
+                <>
+                  <div className="accordian-container">
+                    <Accordion
+                      className={`accord-dark ${classes.accord}`}
+                      expanded={expanded === item.panel}
+                      onChange={handleChange(`${item.panel}`)}
+                      style={{
+                        margin: "0",
+                        border: "1px solid #CECECE",
+                      }}
                     >
-                      <AccordionSummary
-                        style={{ color: "#1C1E21" }}
-                        expandIcon={
-                          <div
-                            className={
-                              isDarkTheme ? "accord-dark" : "accord-light"
-                            }
-                          >
-                            <MdKeyboardArrowUp
-                              style={{ color: "#c60043", fontSize: "27px" }}
-                            />
-                          </div>
-                        }
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
+                      <div
+                        className={useColorMode().colorMode == "dark" ? "accord-dark" : "accord-light"}
                       >
-                        <h3 className="faq-question">
-                          <i
-                            className="fa fa-question-circle"
-                            aria-hidden="true"
-                          ></i>
-                          &nbsp; &nbsp;{item.question}
-                        </h3>
-                      </AccordionSummary>
-                    </div>
-                    <div className={isDarkTheme ? "accord-dark" : null}>
-                      <AccordionDetails>
-                        <Typography style={{ color: "#1C1E21" }}>
-                          <div id={isDarkTheme ? "white-text" : null}>
-                            {item.answer}
-                          </div>
-                        </Typography>
-                      </AccordionDetails>
-                    </div>
-                  </Accordion>
-                </div>
-              </>
-            );
-          })}
+                        <AccordionSummary
+                          style={{ color: "#1C1E21" }}
+                          expandIcon={
+                            <div
+                              className={
+                                useColorMode().colorMode == "dark" ? "accord-dark" : "accord-light"
+                              }
+                            >
+                              <MdKeyboardArrowUp
+                                style={{ color: "#c60043", fontSize: "27px" }}
+                              />
+                            </div>
+                          }
+                          aria-controls="panel1a-content"
+                          id="panel1a-header"
+                        >
+                          <h3 className="faq-question">
+                            &nbsp; &nbsp;{item.question}
+                          </h3>
+                        </AccordionSummary>
+                      </div>
+                      <div className={useColorMode().colorMode == "dark" ? "accord-dark" : null}>
+                        <AccordionDetails>
+                          <Typography style={{ color: "#1C1E21" }} component="span">
+                            <div id={useColorMode().colorMode == "dark" ? "white-text" : null}>
+                              &nbsp; &nbsp;{item.answer}
+                            </div>
+                          </Typography>
+                        </AccordionDetails>
+                      </div>
+                    </Accordion>
+                  </div>
+                </>
+              );
+            })
+          )}
         </div>
       </div>
     </>
