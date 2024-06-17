4d563e38d7f75d586be63a99cb268bc6c79517a5
Raphiel Rollerscaperers <rapherion@raphielgang.org>
2024-06-17 17:07:41 +0700

    index: Accordion is BrowserOnly
    Signed-off-by: Raphiel Rollerscaperers <rapherion@raphielgang.org>

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Layout from '@theme/Layout';
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Accordian from "../components/Accordian/Accordian";
@@ -11,11 +12,15 @@ export default function Home() {
       title={`Hello from ${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <main>
-        <Router>
-          <Routes>
-            <Route path="/" component={Accordian} />
-          </Routes>
-        </Router>
+        <BrowserOnly fallback={<div>Loading...</div>}>
+          {() => (
+            <Router>
+              <Routes>
+                <Route path="/" element={<Accordian />} />
+              </Routes>
+            </Router>
+          )}
+        </BrowserOnly>
       </main>
     </Layout>
   );
